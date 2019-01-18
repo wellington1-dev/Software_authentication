293342e485d143d5c961707df9b626476773b317
Wellington <wellington.developer1@gmail.com>
2019-01-18 13:31:27 -0800

    First commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // configure the code below with your username, password and mlab database information
+    database: 'mongodb://localhost:27017/rain_test',   //prod
+    //database: 'mongodb://localhost:27017/meanauth',    //dev
+    secret: 'yoursecret'
+  }
